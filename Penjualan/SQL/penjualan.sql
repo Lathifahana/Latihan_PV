49f173827c548b6ec3b4cfebdce48b1d975e3698
Lathifah_1597 <lathifahana.1@gmail.com>
2025-06-01 14:29:19 +0700

    Add files via upload

--- Penjualan/SQL/penjualan.sql
+++ Penjualan/SQL/penjualan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 02:07 PM
+-- Generation Time: Jun 01, 2025 at 09:25 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,9 +40,17 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`kd_brg`, `nm_brg`, `jenis`, `hargabeli`, `hargajual`) VALUES
-('KD0001', 'Soto', 'Makanan', '5000', '7000'),
-('KD0002', 'Le mineral', 'Minuman', '3000', '5000'),
-('KD0003', 'Indomie', 'Makanan', '2000', '4000');
+('IB0001', 'Soto', 'Makanan', '5000', '7000'),
+('IB0002', 'Le Mineral', 'Minuman', '3000', '5000'),
+('IB0003', 'Indomie', 'Makanan', '2000', '4000'),
+('IB0004', 'Nipis Madu', 'Minuman', '2000', '3000'),
+('IB0005', 'Sari Roti', 'Makanan', '3000', '5000'),
+('IB0006', 'Pocari Sweat', 'Minuman', '3000', '8000'),
+('IB0007', 'Es Jeruk', 'Minuman', '20000', '6000'),
+('IB0008', 'Pancong', 'Makanan', '3000', '9000'),
+('IB0009', 'Spaghetti', 'Makanan', '15000', '30000'),
+('IB0010', 'Teh Tarik', 'Minuman', '2000', '5000'),
+('IB0011', 'Milo', 'Minuman', '3000', '6000');
 
 -- --------------------------------------------------------
 
@@ -57,6 +65,23 @@ CREATE TABLE `isi` (
   `hargajual` bigint(20) NOT NULL,
   `qty` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `isi`
+--
+
+INSERT INTO `isi` (`id_nota`, `kd_brg`, `hargabeli`, `hargajual`, `qty`) VALUES
+('IN0006', 'IB0003', 2000, 4000, 2),
+('IN0007', 'IB0003', 2000, 4000, 2),
+('IN0007', 'IB0010', 2000, 5000, 2),
+('IN0008', 'IB0010', 2000, 5000, 2),
+('IN0001', 'IB0001', 5000, 7000, 1),
+('IN0002', 'IB0003', 2000, 4000, 2),
+('IN0003', 'IB0011', 3000, 6000, 3),
+('IN0004', 'IB0009', 15000, 30000, 1),
+('IN0005', 'IB0007', 20000, 6000, 1),
+('IN0009', 'IB0001', 5000, 7000, 1),
+('IN0009', 'IB0007', 20000, 6000, 1);
 
 -- --------------------------------------------------------
 
@@ -79,9 +104,12 @@ CREATE TABLE `kasir` (
 --
 
 INSERT INTO `kasir` (`id_kasir`, `nama_kasir`, `jenis_kelamin`, `no_telepon`, `agama`, `alamat`, `password`) VALUES
-('KID001', 'Kian', 'Laki-Laki', '0896383632', 'Islam', 'Bekasi', '1234'),
-('KID002', 'Nia', 'Perempuan', '0879233267', 'Islam', 'Jakarta', '1243'),
-('KID003', 'Kenzo', 'Laki-Laki', '0897567812', 'Kristen', 'Bogor', '1212');
+('IK0001', 'Lathifah H.N', 'Perempuan', '081312234512', 'Islam', 'Jakarta', '1212'),
+('IK0002', 'Alya', 'Perempuan', '085712346735', 'Islam', 'Bintara', '1010'),
+('IK0003', 'Danis', 'Laki-Laki', '081237485821', 'Islam', 'Bekasi', '0202'),
+('IK0004', 'Naufal', 'Laki-Laki', '081236273586', 'Islam', 'Depok', '1818'),
+('IK0005', 'John', 'Laki-Laki', '081262341241', 'Kristen', 'Milan', '0303'),
+('IK0006', 'Yasmina', 'Perempuan', '089612345678', 'Islam', 'Kutai', '1313');
 
 -- --------------------------------------------------------
 
@@ -95,6 +123,21 @@ CREATE TABLE `nota` (
   `id` varchar(6) NOT NULL,
   `id_kasir` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nota`
+--
+
+INSERT INTO `nota` (`id_nota`, `tgl_nota`, `id`, `id_kasir`) VALUES
+('IN0001', '2025-05-24', 'IP0001', 'IK0001'),
+('IN0002', '2025-05-24', 'IP0002', 'IK0001'),
+('IN0003', '2025-05-24', 'IP0004', 'IK0002'),
+('IN0004', '2025-05-24', 'IP0005', 'IK0003'),
+('IN0005', '2025-05-24', 'IP0003', 'IK0004'),
+('IN0006', '2025-05-28', 'IP0001', 'IK0001'),
+('IN0007', '2025-05-28', 'IP0004', 'IK0001'),
+('IN0008', '2025-06-01', 'IP0002', 'IK0001'),
+('IN0009', '2025-06-01', 'IP0005', 'IK0002');
 
 -- --------------------------------------------------------
 
@@ -115,8 +158,12 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id`, `nmplgn`, `jenis`, `telepon`, `alamat`) VALUES
-('ID0001', 'Lathifah Hana', 'Perempuan', '081365728741', 'Indonesia'),
-('ID0002', 'Kirana', 'Perempuan', '089745672435', 'Jakarta');
+('IP0001', 'Lathifah H.N', 'Perempuan', '081312341212', 'Depok'),
+('IP0002', 'Kirana', 'Perempuan', '082145674545', 'Jakarta'),
+('IP0003', 'Swara', 'Laki-Laki', '085678907878', 'Bogor'),
+('IP0004', 'Bimo', 'Laki-Laki', '089636527421', 'Tangerang'),
+('IP0005', 'Rendi', 'Laki-Laki', '087632587213', 'Banten'),
+('IP0006', 'Yasmin', 'Perempuan', '081234561789', 'Bekasi');
 
 --
 -- Indexes for dumped tables
@@ -129,6 +176,13 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`kd_brg`);
 
 --
+-- Indexes for table `isi`
+--
+ALTER TABLE `isi`
+  ADD KEY `fk_isi_nota` (`id_nota`),
+  ADD KEY `fk_isi_barang` (`kd_brg`);
+
+--
 -- Indexes for table `kasir`
 --
 ALTER TABLE `kasir`
@@ -138,13 +192,33 @@ ALTER TABLE `kasir`
 -- Indexes for table `nota`
 --
 ALTER TABLE `nota`
-  ADD PRIMARY KEY (`id_nota`);
+  ADD PRIMARY KEY (`id_nota`),
+  ADD KEY `fk_nota_kasir` (`id_kasir`),
+  ADD KEY `fk_nota_pelanggan` (`id`);
 
 --
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `isi`
+--
+ALTER TABLE `isi`
+  ADD CONSTRAINT `fk_isi_barang` FOREIGN KEY (`kd_brg`) REFERENCES `barang` (`kd_brg`),
+  ADD CONSTRAINT `fk_isi_nota` FOREIGN KEY (`id_nota`) REFERENCES `nota` (`id_nota`);
+
+--
+-- Constraints for table `nota`
+--
+ALTER TABLE `nota`
+  ADD CONSTRAINT `fk_nota_kasir` FOREIGN KEY (`id_kasir`) REFERENCES `kasir` (`id_kasir`),
+  ADD CONSTRAINT `fk_nota_pelanggan` FOREIGN KEY (`id`) REFERENCES `pelanggan` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
